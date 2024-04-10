327e4da1430add7ecaa5a5a2507f63ce7eeb5119
Prachi Singh Sherawat <prachisinghsherawat@gmail.com>
2024-04-10 23:05:15 +0530

    feat: add admin and student dashboard in home

---
+++ src/Components/home/StudentDashboard.jsx
@@ -0,0 +1,95 @@
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import TextInput from "../base/TextInput";
+
+export default function StudentDashboard() {
+  return (
+    <form className="max-w-3xl mx-auto my-24">
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">
+            Profile
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            This information will be displayed publicly so be careful what you
+            share.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-4">
+            <div className="sm:col-span-4">
+              <TextInput
+                label="Name*"
+                type="text"
+                placeholder="Enter your name"
+              />
+            </div>
+
+            <div className="sm:col-span-4">
+              <TextInput
+                label="Email*"
+                type="email"
+                placeholder="Enter your email"
+              />
+            </div>
+
+            <div className="sm:col-span-4">
+              <TextInput
+                label="Contact Number*"
+                type="number"
+                placeholder="Enter your contact number"
+              />
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="cover-photo"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Resume*
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div className="text-center">
+                  <PhotoIcon
+                    className="mx-auto h-12 w-12 text-gray-300"
+                    aria-hidden="true"
+                  />
+                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600"
+                    >
+                      <span>Upload your resume</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                      />
+                    </label>
+                  </div>
+                  <p className="text-xs leading-5 text-gray-600">
+                    PNG, JPG, PDF up to 10MB
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+}
