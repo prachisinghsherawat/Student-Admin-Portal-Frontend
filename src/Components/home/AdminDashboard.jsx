e67a39c39638bf852d89b29bf4ab2c5682339fce
Prachi Singh Sherawat <prachisinghsherawat@gmail.com>
2024-04-11 14:12:19 +0530

    feat: install axios package

--- src/Components/home/AdminDashboard.jsx
+++ src/Components/home/AdminDashboard.jsx
@@ -73,14 +73,7 @@ export default function AdminDashboard() {
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                       {person.role}
                     </td>
-                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
-                      <a
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Edit<span className="sr-only">, {person.name}</span>
-                      </a>
-                    </td>
+                    
                   </tr>
                 ))}
               </tbody>
