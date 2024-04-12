7443311ce28e41c15f2b7a2be9d2a49287851174
Prachi Singh Sherawat <prachisinghsherawat@gmail.com>
2024-04-12 12:18:54 +0530

    feat: add text input component

--- src/Components/base/TextInput.jsx
+++ src/Components/base/TextInput.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 function TextInput(props) {
+  const { label, type, name, id, placeholder, error, onChange } = props;
 
-   const { label, type, name, id, placeholder } = props
+  const handleInputChange = (e) => {
+    if (onChange && error) {
+      onChange({ ...e, target: { ...e.target, name } });
+    } else if (onChange) {
+      onChange(e);
+    }
+  };
 
   return (
     <div>
@@ -17,9 +24,13 @@ function TextInput(props) {
           type={type}
           name={name}
           id={id}
-          className="block w-full rounded-md border-0 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+          className={`block w-full rounded-md border-0 px-2.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 ${
+            error ? "border-red-500" : ""
+          }`}
           placeholder={placeholder}
+          onChange={handleInputChange}
         />
+        {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
       </div>
     </div>
   );
