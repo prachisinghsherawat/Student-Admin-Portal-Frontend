46df8540116da64e0142235006223b5ba8d425f3
Prachi Singh Sherawat <prachisinghsherawat@gmail.com>
2024-04-15 11:43:22 +0530

    feat: add routes

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -3,12 +3,14 @@ import LoginPage from "../Components/Login";
 import { Route,Routes } from "react-router-dom";
 import AdminDashboard from "../Components/home/AdminDashboard";
 import StudentDashboard from "../Components/home/StudentDashboard";
+import TransactionDetails from "../Components/TransactionDetails";
 
 export default function AllRoutes(){
     return (
       <>
         <Routes>
-          <Route path="/student" element={<StudentDashboard/>} />
+          <Route path="/" element={<TransactionDetails />} />
+          <Route path="/student" element={<StudentDashboard />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
